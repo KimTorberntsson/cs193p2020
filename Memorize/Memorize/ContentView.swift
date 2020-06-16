f8ad29b6ea4d56a9a46129d798aeef49e92e1191
Kim Torberntsson <kimtorberntsson@gmail.com>
2020-06-16 19:16:24 +0200

    Initial Commit

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Kim Torberntsson on 2020-06-16.
+//  Copyright © 2020 Kim Torberntsson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
