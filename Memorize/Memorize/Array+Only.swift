f6c119d58b07c01e371de690b520dff322d902f4
Kim Torberntsson <kimtorberntsson@gmail.com>
2020-06-18 19:49:18 +0200

    Minor cleanup

--- Memorize/Memorize/Array+Only.swift
+++ Memorize/Memorize/Array+Only.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+
 extension Array {
     var only: Element? {
         count == 1 ? first : nil
--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2020 Kim Torberntsson. All rights reserved.
 //
 
-import Foundation
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
