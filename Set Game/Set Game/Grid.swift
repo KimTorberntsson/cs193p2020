a07dc7684e08a648c06679a2346c182bef89f199
Kim Torberntsson <kim.torberntsson@gmail.com>
2020-06-20 14:36:37 +0200

    Added grid to project

---
+++ Set Game/Set Game/Grid.swift
@@ -0,0 +1,39 @@
+//
+//  Grid.swift
+//  Memorize
+//
+//  Created by Kim Torberntsson on 2020-06-17.
+//  Copyright © 2020 Kim Torberntsson. All rights reserved.
+//
+
+import SwiftUI
+
+struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
+    private var items: [Item]
+    private var viewForItem: (Item) -> ItemView
+    
+    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.viewForItem = viewForItem
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            self.body(for: GridLayout(itemCount: self.items.count, in: geometry.size))
+        }
+    }
+    
+    private func body(for layout: GridLayout) -> some View {
+        ForEach(items) { item in
+            self.body(for: item, in: layout)
+        }
+    }
+    
+    private func body(for item: Item, in layout: GridLayout) -> some View {
+        let index = self.items.firstIndex(matching: item)
+        return self.viewForItem(item)
+            .frame(width: layout.itemSize.width, height: layout.itemSize.height)
+            .position(layout.location(ofItemAt: index!))
+    }
+
+}
