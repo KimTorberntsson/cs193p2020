e2b597dcc7d0b2f7c84ba8b8fdd8edb4d5403421
Kim Torberntsson <kim.torberntsson@gmail.com>
2020-06-20 14:27:20 +0200

    Initial commit for the set game

---
+++ Set Game/Set Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Kim Torberntsson on 2020-06-20.
+//  Copyright © 2020 Kim Torberntsson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set Game/Set Game/ShapeSetGame.swift
@@ -0,0 +1,9 @@
+//
+//  ShapeSetGame.swift
+//  Set Game
+//
+//  Created by Kim Torberntsson on 2020-06-20.
+//  Copyright © 2020 Kim Torberntsson. All rights reserved.
+//
+
+import Foundation
