6c347775315134d5ed0647a7a0845947eb490bc0
Kim Torberntsson <kim.torberntsson@gmail.com>
2020-06-21 13:08:13 +0200

    Made all shapes have the same size

--- Set Game/Set Game/ShapeSetGameView.swift
+++ Set Game/Set Game/ShapeSetGameView.swift
@@ -30,10 +30,16 @@ struct CardView: View {
                 RoundedRectangle(cornerRadius: cornerRadius)
                     .stroke(lineWidth: lineWidth)
                 VStack {
-                    ForEach(0..<convertNumber(from: card.number)) { _ in
-                        self.getView(from: self.card)
+                    ForEach(0..<3) { index in
+                        if index < self.convertNumber(from: self.card.number) {
+                            self.getView(from: self.card)
+                        } else {
+                            EmptyView()
+                        }
                     }
-                }.padding()
+                }
+                .padding(.horizontal, horizontalExtraPaddingForShapes)
+                .padding(cardPadding)
             }
         }
         .foregroundColor(convertColor(from: card.color))
@@ -42,7 +48,7 @@ struct CardView: View {
     
     @ViewBuilder
     func getView(from card: SetGameCard) -> some View {
-        VStack {
+        Group {
             if card.type == .diamond {
                 if card.shading == .open {
                     Circle().stroke(lineWidth: 3)
@@ -51,9 +57,9 @@ struct CardView: View {
                 }
             } else if card.type == .oval {
                 if card.shading == .open {
-                    Ellipse().stroke(lineWidth: 3)
+                    Capsule().stroke(lineWidth: 3)
                 } else {
-                    Ellipse()
+                    Capsule()
                 }
             } else if card.type == .rectangle {
                 if card.shading == .open {
@@ -63,7 +69,7 @@ struct CardView: View {
                 }
             }
         }
-        .padding(shapePadding)
+        .padding(.vertical, shapePadding)
         .aspectRatio(3/2, contentMode: .fit)
         .opacity(card.shading == .striped ? stripedOpacity : 1)
     }
@@ -92,6 +98,8 @@ struct CardView: View {
     let cardPadding: CGFloat = 5
     let shapePadding: CGFloat = 5
     
+    // This is a way of having all shapes be of the same size
+    var horizontalExtraPaddingForShapes : CGFloat { 4*shapePadding }
 }
 
 // MARK: - Content Preview
