ade78cdda7ac52e8cfbcda6841bf0905f855ddd8
Kim Torberntsson <kim.torberntsson@gmail.com>
2020-06-28 15:04:14 +0200

    Added code from lecture 7

---
+++ EmojiArt/EmojiArt/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Kim Torberntsson on 2020-06-26.
+//  Copyright © 2020 Kim Torberntsson. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    
+    @ObservedObject var document: EmojiArtDocument
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
---
+++ EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,64 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Kim Torberntsson on 2020-06-26.
+//  Copyright © 2020 Kim Torberntsson. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    
+    static let palette = "🥑🍊🧀🌭🥙🥘🍟"
+    
+    @Published private var emojiArt = EmojiArt()
+    
+    @Published private(set) var backgroundImage : UIImage?
+    
+    var emojis: [EmojiArt.Emoji] { emojiArt.emojis }
+    
+    //MARK: - Intents
+    
+    func addEmoji(emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+    
+    func moveEmoji(emoji: EmojiArt.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].size =  Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrEven))
+        }
+    }
+    
+    func setBackGroundURL(url: URL?) {
+        emojiArt.backgroundURL = url?.imageURL
+        fetchBackgroundImageData()
+    }
+    
+    private func fetchBackgroundImageData() {
+        backgroundImage = nil
+        if let url = self.emojiArt.backgroundURL {
+            DispatchQueue.global(qos: .userInitiated).async {
+                if let imageData = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        if url == self.emojiArt.backgroundURL {
+                            self.backgroundImage = UIImage(data: imageData)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension EmojiArt.Emoji {
+    var fontSize: CGFloat { CGFloat(size) }
+    var location: CGPoint { CGPoint(x: CGFloat(x), y: CGFloat(y)) }
+}
